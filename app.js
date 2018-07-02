2e918c97c06c455f45e1367d49a4b0f4b3c57360
Rishav Bhagat <rishavb123@gmail.com>
2018-07-02 11:35:55 -0400

    added

--- app.js
+++ app.js
@@ -15,16 +15,37 @@ firebase.auth().onAuthStateChanged(function(user) {
 });
 
 let data;
-let ref = firebase.database().ref(firebase.auth().uid);
-ref.on("value", snap => {
-  data = snap.val();
-});
+
+let name = data.name;
 
 let balance = 0;
 let balanceOutput = document.getElementById("balanceOutput");
 let salary = parseInt(data.salary);
+let salaryOutput = document.getElementById("salaryOutput");
+
+let ref = firebase.database().ref(firebase.auth().uid);
+ref.on("value", snap => {
+  data = snap.val();
+  balance = data.balance;
+  salary = data.salary;
+  name = data.name;
+});
+
 if(!salary)
 {
     salary = Math.floor(Math.random()*90000)+10000;
     ref.child("salary").set(salary);
+    salaryOutput.innerHTML = salary;
 }
+
+if(!name)
+{
+    name =  "Rishav";
+    ref.child("salary").set(salary);
+}
+
+function payday()
+{
+   balance += salary
+}
+
