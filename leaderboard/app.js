56736b4f4dd7750afc59508573c561b46a7f73b3
Kihtrak <karthikandsankar@gmail.com>
2018-07-02 15:42:05 -0400

    test

--- leaderboard/app.js
+++ leaderboard/app.js
@@ -1,3 +1,8 @@
-
-
-document.getElementById('tbody').innerHTML = '<tr><td>You earned '+s+' dollars from your salary</td></tr>' + document.getElementById('tbody').innerHTML;
+let ref = firebase.database().ref();
+ref.on("value", snap => {
+   let temp = "";
+   for(prop in snap.val()){
+       temp += '<tr><td>'+snap.val()[prop][name]+' dollars from your salary</td></tr>';
+   }
+    document.getElementById('tbody').innerHTML = temp;
+});
